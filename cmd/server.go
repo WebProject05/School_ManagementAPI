e1d27b9b802c7508682a1d86393d3865af7af966
Chopperla Santosh <chopperla2005@gmail.com>
2026-03-24 18:40:21 +0530

    Modified Post Method

--- cmd/server.go
+++ cmd/server.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
+type user struct {
+	Name string `json:"name"`
+	Age string `json:"age"`
+	City string `json:"city"`
+}
+
 func main() {
 	port := ":3000"
-
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
@@ -18,15 +25,66 @@ func main() {
 		w.Write([]byte("Hello from the server!"))
 	})
 
-
 	http.HandleFunc("/teachers", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			w.Write([]byte("Hello GET Method on the teachers route"))
+		switch r.Method {
+		case http.MethodGet:
+			w.Write([]byte("Read (GET) teachers"))
 			return
+
+		case http.MethodPost:
+			// Parsing the form data from the user
+			err := r.ParseForm()
+			if err != nil {
+				http.Error(w, "Error Parsing the form", http.StatusBadRequest)
+				log.Fatalln("Error parsing the input data:", err)
+				return
+			}
+
+			// Preparing the response data
+			response := make(map[string]interface{})
+			for key, value := range r.Form {
+				response[key] = value[0]
+			}
+
+			// RAW data parsing with json
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				return
+			}
+			defer r.Body.Close()
+			fmt.Println("Raw Date:",string(body))
+
+
+			// UnMarshling the raw body
+			var userInstance user
+			err = json.Unmarshal(body, &userInstance)
+			if err != nil {
+				return
+			}
+
+			fmt.Println("UnMarshaled JSON:",userInstance)
+
+			fmt.Println("Form data:", r.Form)
+			w.Write([]byte("Create (POST) teacher"))
+			return
+
+		case http.MethodPut:
+			w.Write([]byte("Update (PUT) teacher"))
+			return
+
+		case http.MethodPatch:
+			w.Write([]byte("Partial Update (PATCH) teacher"))
+			return
+
+		case http.MethodDelete:
+			w.Write([]byte("Delete (DELETE) teacher"))
+			return
+
+		default:
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		}
 		w.Write([]byte("Hello from the teachers route!"))
 	})
-
 
 	http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello from the students route!"))
